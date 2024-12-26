7b90e158c6cce3af30160d7ecfbf2fe0f661b646
Maksim Barankov <barankovmps4@gmail.com>
2024-12-26 19:27:25 +0600

    добавил тесты добавления в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,12 @@ class MainPageLocators():
 class LoginPageLocators():
      LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
      REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    SUBMIT_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main h1 + p")
+    SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alertinner strong")
+    SUCCESS_MESSAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, "#messages .alert-info .alertinner p strong")
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,37 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        link = self.browser.find_element(*ProductPageLocators.SUBMIT_BUTTON)
+        link.click()
+
+    def should_be_correct_book_name_in_message(self):
+        page_name = self.get_product_name()
+        alert_name = self.get_success_message_name()
+        assert page_name == alert_name, "book titles don't match"
+
+    def should_be_correct_book_price_in_message(self):
+        page_price = self.get_product_price()
+        message_name = self.get_success_message_price()
+        assert page_price == message_name, "the prices of the books do not match"
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_success_message_name(self):
+        return self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def get_success_message_price(self):
+        return self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_PRICE).text
+
+
+
+
+
+
+
