8dd9c4faba8d7a00c1fc1df21d3b6bbcb598b2cf
Maksim Barankov <barankovmps4@gmail.com>
2024-12-28 19:18:13 +0600

    код подготовлен к ревью

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,7 @@ from .pages.basket_page import BasketPage
 import pytest
 import time
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer', [0,1,2,3,4,5,6,pytest.param(7, marks=pytest.mark.xfail),8,9])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
@@ -42,6 +43,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page (browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -50,6 +52,7 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/es/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -70,6 +73,7 @@ class TestUserAddToBasketFromProductPage():
         self.page.register_new_user(email, password)
         self.page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser, link)
